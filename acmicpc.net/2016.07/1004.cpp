3a767e8182a5a74a833c6384bf38c5c101e4eefb
Jeremy <sangmookoh@gmail.com>
2016-07-05 16:55:20 +0900

    7.5

---
+++ acmicpc.net/2016.07/1004.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+int POW(int num)
+{
+	return num*num;
+}
+int main()
+{
+	int T; cin >> T;
+	while (T--)
+	{
+		pair<int, int> start, des, planet;
+		int n, cnt = 0, r;
+
+		cin >> start.first >> start.second >> des.first >> des.second;
+		cin >> n;
+		while (n--)
+		{
+			cin >> planet.first >> planet.second >> r;
+			int d1 = POW(start.first - planet.first) + POW(start.second - planet.second);
+			int d2 = POW(des.first - planet.first) + POW(des.second - planet.second);
+
+			if (d1 <= POW(r) && d2 <= POW(r)) continue;
+
+			if (d1 <= POW(r)) cnt++;
+			if (d2 <= POW(r)) cnt++;
+		}
+		cout << cnt << endl;
+	}
+	return 0;
+}
---
+++ acmicpc.net/2016.07/1562.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+#define MOD 1000000000
+int dp[105][10][1 << 10], n, ans;
+int dfs(int idx, int num, int bit)
+{
+	if (dp[idx][num][bit]) return dp[idx][num][bit];
+	if (idx == n) return bit == (1 << 10) - 1 ? 1 : 0;
+
+	int res = 0;
+
+	if (num + 1 < 10) res += dfs(idx + 1, num + 1, bit | 1 << (num + 1));
+	if (num - 1 >= 0) res += dfs(idx + 1, num - 1, bit | 1 << (num - 1));
+	res %= MOD;
+	return dp[idx][num][bit] = res;
+}
+int main()
+{
+	cin >> n;
+	for (int i = 1; i < 10; i++)
+	{
+		ans += dfs(1, i, 1 << i);
+		ans %= MOD;
+	}
+	cout << ans << endl;
+	return 0;
+}
