b621c2e7ae52e10aa7d30d31dbe4d37e6a1acb58
Jeremy <sangmookoh@gmail.com>
2016-07-07 00:00:16 +0900

    7.6

---
+++ acmicpc.net/2016.07/1065.cpp
@@ -0,0 +1,20 @@
+﻿#includeiostream
+using namespace std;
+int main()
+{
+	int n, cnt = 0;
+	cin  n;
+	for (int i = 1; i = n; i++)
+	{
+		if (i  100) cnt++;
+		else
+		{
+			int a = i  100;
+			int b = (i  10) % 10;
+			int c = i % 10;
+			if (b - a == c - b) cnt++;
+		}
+	}
+	cout  cnt  endl;
+	return 0;
+}
---
+++ acmicpc.net/2016.07/1072.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	double x, y, z;
+	while (cin >> x >> y)
+	{
+		z = (y * 100) / x;
+		z = (int)z;
+		if (z >= 99) cout << -1 << endl;
+		else cout << (long long)ceil(((((z + 1)*x) / 100) - y) * (100 / (100 - z - 1))) << endl;
+	}
+	return 0;
+}
