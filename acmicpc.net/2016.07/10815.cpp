2f55bb125c83022199550b0cff0716a9821fed17
Jeremy <sangmookoh@gmail.com>
2016-07-08 00:05:06 +0900

    7.7

---
+++ acmicpc.net/2016.07/10815.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+int arr[500005], n, m, input;
+int main()
+{
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)  scanf("%d", &arr[i]);
+	sort(arr, arr + n);
+	scanf("%d", &m);
+	while (m--)
+	{
+		scanf("%d", &input);
+
+		int start = 0;
+		int end = n - 1;
+		int ans = 0;
+		while (start <= end)
+		{
+			int middle = (start + end) / 2;
+
+			if (arr[middle] == input)
+			{
+				ans = 1;
+				break;
+			}
+
+			if (arr[middle] < input) start = middle + 1;
+			if (arr[middle] > input) end = middle - 1;
+
+		}
+
+		printf("%d ", ans);
+	}
+	return 0;
+}
---
+++ acmicpc.net/2016.07/6527.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<string>
+#include<set>
+using namespace std;
+int gcd(int a, int b)
+{
+	if (a%b == 0) return b;
+	else return gcd(b, a%b);
+}
+int main()
+{
+	set<string> state;
+	string input, word = "";
+	int input_len, game = 0, sz = 0;
+	while (getline(cin, input))
+	{
+		input += '.';
+		input_len = input.size();
+		for (int i = 0; i < input_len; i++)
+		{
+			if (isalpha(input[i]))
+				word += toupper(input[i]);
+			else
+			{
+				if (word.compare("BULLSHIT") == 0)
+				{
+					game++;
+					sz += state.size();
+					state.clear();
+				}
+				else if (word.size()) state.insert(word);
+				word = "";
+			}
+		}
+	}
+	int g = gcd(sz, game);
+	cout << sz / g << " / " << game / g << endl;
+	return 0;
+}
