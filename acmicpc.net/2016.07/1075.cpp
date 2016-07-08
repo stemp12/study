1d3aa43d80806f7d93ff67af1578b6bd84ddbb24
Jeremy <sangmookoh@gmail.com>
2016-07-08 16:54:37 +0900

    7.8

---
+++ acmicpc.net/2016.07/1075.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int n, f;
+	cin >> n >> f;
+	n = (n / 100) * 100;
+	for (int i = 0; i < 100; i++)
+		if ((n + i) % f == 0)
+		{
+			printf("%02d", i);
+			break;
+		}
+	return 0;
+}
---
+++ acmicpc.net/2016.07/2188.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+#define MAX 500
+using namespace std;
+
+vector<vector<int> > v(MAX);
+bool chk[MAX];
+int bmatch[MAX], n, m;
+
+bool dfs(int pos)
+{
+	if (chk[pos]) return false;
+	chk[pos] = true;
+
+	for (int i = 0; i < v[pos].size(); i++)
+	{
+		int next = v[pos][i];
+		if (bmatch[next] == -1 || dfs(bmatch[next]))
+		{
+			bmatch[next] = pos;
+			return true;
+		}
+	}
+	return false;
+}
+int bmp()
+{
+	fill(bmatch, bmatch + MAX, -1);
+	int match = 0;
+	for (int i = 0; i < n; i++)
+	{
+		fill(chk, chk + MAX, false);
+		if (dfs(i)) match++;
+	}
+	return match;
+}
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		int sz, like; 
+		cin >> sz;
+		for (int k = 0; k < sz; k++)
+		{
+			cin >> like;
+			v[i].push_back(like - 1);
+		}
+	}
+	cout << bmp() << endl;
+	return 0;
+}
