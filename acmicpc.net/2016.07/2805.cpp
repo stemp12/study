886322a7cc10573fe647568aad0e4fa592e09c3a
Jeremy <sangmookoh@gmail.com>
2016-07-12 15:51:33 +0900

    7.12

---
+++ acmicpc.net/2016.07/2805.cpp
@@ -0,0 +1,65 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+int arr[1000003];
+int n, m;
+int my_search(int start, int end, bool flag, int stan)
+{
+	long long sum = 0;
+	for (int i = end; i >= start; i--)
+	{
+		if(flag) sum += arr[i]-arr[start];
+		else sum += arr[i] - stan;
+
+		if (sum > m) return 1;
+	}
+	if (sum == m) return 0;
+	else return -1;
+}
+int bsearch(int &left, int &right, bool flag, int stan)
+{
+	int middle, state;
+	while (left <= right)
+	{
+		middle = (left + right) / 2;
+		int state;
+		if(flag) state= my_search(middle, n, flag, 0);
+		else state = my_search(stan, n, flag, middle);
+
+		if (state == 0)
+		{
+			if (flag) return arr[middle];
+			else return middle;
+		}
+		else if (state == 1) left = middle + 1;
+		else right = middle - 1;
+	}
+
+	if(flag) return 0;
+	else
+	{
+		state = my_search(stan, n, flag, middle);
+		if (state == -1) return middle - 1;
+		else return middle;
+	}
+}
+int main()
+{
+	scanf("%d %d", &n, &m);
+	for (int i = 1; i <= n; i++)
+		scanf("%d", &arr[i]);
+	sort(arr, arr + n + 1);
+	
+	int left = 0, right = n;
+	int ans = bsearch(left, right, true, 0);
+
+	if (ans) cout << ans << endl;
+	else
+	{
+		if (right < left) swap(left, right);
+		int nl = arr[left], nr = arr[right];
+		ans = bsearch(nl, nr, false, right);
+		printf("%d", ans);
+	}
+	return 0;
+}
---
+++ acmicpc.net/2016.07/9933.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+string rev(string str)
+{
+	int len = str.length();
+	string temp = "";
+	for (int i = len - 1; i >= 0; i--)
+		temp += str[i];
+	return temp;
+}
+int main()
+{
+	int n; cin >> n;
+	string input;
+	map<string, int> m;
+	while (cin >> input)
+	{
+		m[input]++;
+		m[rev(input)]++;
+
+		if (m[input] == 2 || m[rev(input)] == 2) break;
+	}
+	cout << input.length()<<' '<<input[input.length() / 2];
+	return 0;
+}
